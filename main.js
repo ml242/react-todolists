5c6002d49b20757da8360f45730ab766c47ea0f3
matt lucas <mattlucas@matts-MacBook-Pro.local>
2015-08-18 21:34:04 -0400

    convoluted homemade todolist act with react

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
-var TODOS = ["buy food", "make food", "eat food"];
+
+var TodoList = React.createClass({
+
+	render: function(){
+		var createItem = function(todos, index){
+			return(<li data-key={index}>{todos}</li>); 
+		}
+		return (<ul>{this.props.todos.map(createItem)}</ul>);
+	}
+});
 
 var TodoApp = React.createClass({
+
+	getInitialState: function(){
+		return {
+			text: "",
+			todoLists: [],
+			todos: [],
+			place: 0
+		}
+	},
+
+	onChange: function(e) {
+		this.setState({ text: e.target.value });
+	},
+
+	handleClick: function(){
+		this.setState({
+			todos: this.state.todos.concat(this.state.text)
+		});
+	},
+
+	newList: function(){
+		this.setState({
+			todoLists: this.state.todoLists.concat([this.state.todos]),
+			place: this.state.place += 1
+		});
+		console.log(this.state.todoLists)
+		console.log(this.state.place)
+	},
+
     render: function(){
-        return(<div>{this.props.todos}</div>);
+        return (
+        	<div>
+        		<input type="text" value={this.state.text} onChange={this.onChange} /> 
+				<input type="submit" onClick={this.handleClick} />
+        		<TodoList todos={this.state.todos} />
+        		<button onClick={this.newList}>New List</button>
+    		</div>
+    	)
     },
 });
 
-React.render(<TodoApp todos={TODOS} />, document.getElementById('content'));
+React.render(<TodoApp />, document.getElementById('content'));
